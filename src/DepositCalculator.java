ff63af713c64819cae3a1b62eb992bb2b3902d38
Zyrianov-AM <ZyrianovAM@yandex.ru>
2023-02-25 21:35:36 +0500

    исправил замечания ревьювера

--- src/DepositCalculator.java
+++ src/DepositCalculator.java
@@ -2,41 +2,46 @@ import java.util.Scanner;
 
 public class DepositCalculator  {
 
-    double calculateComplexPercent(double a, double y, int d) {
-       double pay = a * Math.pow ( (1 + y / 12), 12 * d );
-          return rounding (pay, 2);
+    double calculateComplexPercent(double amount, double yearRate, int depositPeriod) {
+        double accrual = amount * Math.pow((1 + yearRate / 12), 12 * depositPeriod);
+
+        return roundingAmount(accrual, 2);
     }
 
-     double calculateSimplePercent(double doubleAmount, double double_year_rate, int deposit_period) {
-         return rounding(doubleAmount + doubleAmount * double_year_rate * deposit_period, 2);
+    double calculateSimplePercent(double amount, double yearRate, int depositPeriod) {
+        double accrual = amount + amount * yearRate * depositPeriod;
+
+        return roundingAmount(accrual, 2);
     }
 
-    double rounding(double value, int places) {
-       double ScaLe = Math.pow(10, places);
-       return Math.round(value * ScaLe) / ScaLe;
+    double roundingAmount(double valueAmount, int places) {
+        double scale = Math.pow(10, places);
+
+        return Math.round(valueAmount * scale) / scale;
     }
 
     void runMenu() {
-      int period;
-      int action;
+        int period;
+        int depositType;
+        int amount;
+        double monyStorage =0;
         Scanner scanner = new Scanner(System.in);
+
         System.out.println("Введите сумму вклада в рублях:") ;
-      int amount = scanner.nextInt();
+        amount = scanner.nextInt();
         System.out.println("Введите срок вклада в годах:") ;
         period = scanner.nextInt();
         System.out.println("Выберите тип вклада, 1 - вклад с обычным процентом, 2 - вклад с капитализацией:");
-        action = scanner.nextInt();
-      double monyStorage = 0;
-        if (action == 1) {
+        depositType = scanner.nextInt();
+        if (depositType == 1) {
             monyStorage = calculateSimplePercent(amount, 0.06, period);
-        } else if (action == 2) {
+        } else if (depositType == 2) {
             monyStorage = calculateComplexPercent(amount, 0.06, period);
         }
-      System.out.println("Результат вклада: " + amount + " за " + period + " лет превратятся в " + monyStorage);
+        System.out.println("Результат вклада: " + amount + " за " + period + " лет превратятся в " + monyStorage);
     }
 
     public static void main(String[] args) {
         new DepositCalculator().runMenu();
     }
-
 }
